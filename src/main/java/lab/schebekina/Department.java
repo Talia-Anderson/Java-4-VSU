0877b885cf25dccdd6d4ac7c613578d4c5ec387b
Natalia Shebekina <lady.malfoy1995@yandex.ru>
2023-12-10 17:05:35 +0300

    fourth lab

---
+++ src/main/java/lab/schebekina/Department.java
@@ -0,0 +1,18 @@
+package lab.schebekina;
+
+public class Department
+{
+    private final int id;
+    private final String name;
+
+    public Department(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department = { department.id = " + id + "; name = " + name + " }";
+    }
+}
